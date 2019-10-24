85869c4dff4b500d4baca566a021421c3f60b8ca
Romzesovicc <52004263+Romzesovicc@users.noreply.github.com>
2019-10-24 12:36:16 +0300

    add services and other

---
+++ src/components/Posts.js
@@ -0,0 +1,72 @@
+import React, {Component} from 'react';
+import InstaService from '../services/instaService'
+import User from "./User";
+import ErrorMessage from './Error'
+
+export default class Posts extends Component {
+    InstaService = new InstaService();
+    state ={
+        posts: [],
+        error: false
+    };
+
+    componentDidMount() {
+        this.updatePosts();
+    }
+
+    updatePosts(){
+        this.InstaService.getAllPosts()
+            .then(this.onPostLoaded)
+            .catch(this.onError);
+    }
+
+    onPostLoaded = (posts) =>{
+      this.setState({
+         posts,
+         error: false
+      });
+        console.log(this.state.posts)
+    };
+
+    onError = () => {
+      this.setState({
+        error:true
+      })
+    };
+
+    renderItems(arr){
+      return arr.map(item => {
+        const {name, altname, src, photo, alt, descr, id} = item;
+
+        return (
+            <div key = {id} className = "post">
+                <User src = {photo}
+                      alt = {altname}
+                      name = {name}
+                      min/>
+                <img src = {src} alt = {alt}>
+
+                </img>
+                <div className = "post__name">
+                    {name}
+                </div>
+                <div className = "post__descr">
+                    {descr}
+                </div>
+            </div>
+        )
+    });
+}
+    render() {
+        const {error, posts} = this.state;
+if(error){
+    return <ErrorMessage />
+}
+        const items = this.renderItems(posts);
+        return(
+            <div className = "left">
+                {items}
+            </div>
+        )
+    }
+}
